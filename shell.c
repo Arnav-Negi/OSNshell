5d959a1254398bfe515d44d7b2229ec651967083
Arnav <arnavnegi14@gmail.com>
2022-08-28 16:47:58 +0530

    start

---
+++ shell.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+void shell_loop()
+{
+    char *line, *command;
+    char **args;
+    int status;
+
+    do {
+        
+    }
+}
+
+int main(int argc, char **argv)
+{
+    shell_loop();
+
+    return 0;
+}
